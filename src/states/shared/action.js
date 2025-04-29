c8a393ad82bd50e02d4b97f16e4d9b2f6a7ec359
Andre <andrelim865@gmail.com>
2025-04-29 13:56:21 +0700

    initial commit

---
+++ src/states/shared/action.js
@@ -0,0 +1,24 @@
+import api from '../../utils/api';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveTalksActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+    }
+
+    dispatch(hideLoading());
+  };
+}
+
+export { asyncPopulateUsersAndTalks };
